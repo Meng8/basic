fa312ecf3d69b464241a0d956fe46482fb5f40c6
Meng8 <1098167361@qq.com>
2017-11-28 22:31:15 +0800

    第一次提交

---
+++ src/main/java/com/example/demo/TaijCongfiguration.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+@ConfigurationProperties("pojo")
+public class TaijCongfiguration {
+	
+	private boolean enabled;
+	private InetAddress remoteAddress;
+	private final Security security = new Security();
+
+	@Data
+	public static class Security {
+		private String username;
+		private String password;
+		private List<String> roles = new ArrayList<>(Collections.singleton("USER"));
+
+	}
+}
